ab90ddea47f0475d54c2710e6c4e3b4b14032637
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-18 09:59:27 -0300

    Configurando o composer

---
+++ lib/composer/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '881d9e9ced5410d04607048e0a26fb71ba924bc2',
+        'name' => 'omartins/calendarphp',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'omartins/calendarphp' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '881d9e9ced5410d04607048e0a26fb71ba924bc2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
