d8d0bc25649a825565629633f094b1f10982bdf5
omartins-zs <gabrielmatheus209@gmail.com>
2022-04-19 08:34:02 -0300

    Linkando JS e CSS no Index

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
+<?php include("config/config.php"); ?>
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calendario Agenda com PHP</title>
 
-echo "Hello Gabriel";
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/css/style.css'; ?>">
+    <link rel="stylesheet" href="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.css'; ?>">
+</head>
+<body>
+
+ <div class="calendar"></div>
+
+<script src="<?php echo DIRPAGE.'lib/js/FullCalendar/main.min.js'; ?>"></script>
+<script src="<?php echo DIRPAGE.'lib/js/javascript.js'; ?>"></script>
+</body>
+</html>
